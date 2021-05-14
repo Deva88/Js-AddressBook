cd9c719e7a3cfb4b4c6f7995ea7e2f676ef7dd04
Deva88 <devendra.raj.sdm@gmail.com>
2021-05-14 12:15:33 +0530

    Added: Ability to ensure Valid Contacts

--- AddressBook.js
+++ AddressBook.js
@@ -3,15 +3,6 @@
  *for the initializing the instances set value with the possition */
 
 class Contact{
-    firstName;
-    lastName;
-    address;
-    city;
-    state;
-    zip;
-    phoneNo;
-    email;
-
     // Constructor
     constructor(...params){
         this.firstName = params[0];
@@ -23,7 +14,108 @@ class Contact{
         this.phoneNo = params[6];
         this.email = params[7];
     }
+
+    /* @Description - To validate first name should start with captital character
+     * To validate the name should have atleast 3 char
+     * if name is not start with capital letter and if its not atleast 3 character its throw error
+     * To used throw keyword for the invalid firstName */
+
+    get getfirstName(){ return this._firstName}
+    set firstName(firstName){
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+        if(nameRegex.test(firstName)) {
+            this._firstName = firstName
+        }
+        else throw "Incorrect First Name"
+    }
+
+    /* @Description - To validate last name should start with captital character
+     * To validate the name should have atleast 3 char
+     * if name is not start with capital letter and if its not atleast 3 character its throw error
+     * To used throw keyword for the invalid lastName */
+
+    get lastName(){ return this._lastName}
+    set lastName(lastName){
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+        if(nameRegex.test(lastName)) {
+            this._lastName = lastName
+        }
+        else throw "Incorrect Last Name"
+    }
+
+    /* @Description - to validate address should start with captital letters or small letters
+     * to used throw keyword for the invalid address */
+
+    get address(){ return this._address}
+    set address(address){
+        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
+        if(regex.test(address)) {
+            this._address = address
+        }
+        else throw "Incorrect Address"
+    }
+
+    /* @Description - to validate city should start with captital letters or small letters
+     * to used throw keyword for the invalid city  */
+
+    get city(){ return this._city}
+    set city(city){
+        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
+        if(regex.test(city)) {
+            this._city = city
+        }
+        else throw "Incorrect City"
+    }
+
+    /* @Description - to validate state should start with captital letters or small letters
+     * to used throw keyword for the invalid state  */
+
+    get state(){ return this._state}
+    set state(state){
+        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
+        if(regex.test(state))
+            this._state = state
+        else throw "Incorrect State"
+    }
+
+    /* @Description - to validate zip  should start with numbers 
+     * to used throw keyword for the invalid state  */
+
+    get zip(){ return this._zip}
+    set zip(zip){
+        let pinRegex = RegExp("^[0-9]{3}\s{0,1}[0-9]{3}$")
+        if(pinRegex.test(zip)){
+            this._zip = zip
+        }
+        else throw "Incorrect Zip"
+    }
+
+    /* @Description - to validate phone number should start with numbers to allow max 10 digit
+     * to used throw keyword for the invalid phone number  */
+
+    get phoneNo(){ return this._phoneNo}
+    set phoneNo(phoneNo){
+        let regex = RegExp('[0-9]{2}\\s[0-9]{10}')
+        if(regex.test(phoneNo)) {
+            this._phoneNo = phoneNo
+        }
+        else throw "Incorrect Phone Number"
+    }
+
+    /* @Description - to validate email id  should start with small letter and have one numeric 
+    * its should must have atleast one special character
+    * to used throw keyword for the invalid emailId  
+    */
+
+    get email(){ return this._email}
+    set email(email){
+        let regex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.]([a-z]+){2,}([.]?[a-z]{2})?$')
+        if(regex.test(email)) {
+            this._email = email
+        }
+        else throw "Incorrect Email"
+    }
 }
 
-let contact1 = new Contact("Devendra", "Kumar", "Garkha", "Saran", "Bihar", 841311, 8123273639,  "dev@gmail.com")
+let contact1 = new Contact("Devendra", "Kumar", "Garkha", "Saran", "Bihar", 841311, "91 8123273639",  "deva@gmail.com")
 console.log(contact1);
